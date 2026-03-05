4e70ca9323c72915875d71b0287c611cdb7a1420
Samuel Ojietohamen <seekersoftec@gmail.com>
2026-03-05 14:08:05 +0100

    feat: Enable external VNC access on port 5900 and correct the MT5 terminal command path in supervisord.

--- MT5/assets/xtigervnc.sh
+++ MT5/assets/xtigervnc.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/usr/bin/Xtigervnc -desktop "$VNC_DESKTOP_NAME" -geometry "$VNC_GEOMETRY" -localhost -rfbport 5900 -SecurityTypes None -AlwaysShared -AcceptKeyEvents -AcceptPointerEvents -AcceptSetDesktopSize -SendCutText -AcceptCutText :0
+/usr/bin/Xtigervnc -desktop "$VNC_DESKTOP_NAME" -geometry "$VNC_GEOMETRY" -listen tcp -ac -rfbport 5900 -SecurityTypes None -AlwaysShared -AcceptKeyEvents -AcceptPointerEvents -AcceptSetDesktopSize -SendCutText -AcceptCutText :0
