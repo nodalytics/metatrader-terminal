822008d2e811965ebee157bc233eb4b56a939759
Samuel Ojietohamen <seekersoftec@gmail.com>
2026-03-05 22:07:45 +0100

    refactor: remove MetaTrader5 service and disable auto-login script in server startup.

--- MT5/assets/run-server.sh
+++ MT5/assets/run-server.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Run the auto-login script
-wine python $HOME/auto_login.py
+# wine python $HOME/auto_login.py
 
 # Wait for 2 seconds
-sleep 2
+# sleep 2
 
 # Start the server after the auto-login script completes
 echo "Starting FastAPI Server..."
